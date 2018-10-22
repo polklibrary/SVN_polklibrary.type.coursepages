0880a2f3a6b683ab288888a79aa8d57ec2c9d8f6
hietpasd <hietpasd@uwosh.edu>
2018-10-22 13:13:34 -0500

    adjusted roles

--- polklibrary.type.coursepages/src/polklibrary/type/coursepages/browser/canvas_view.py
+++ polklibrary.type.coursepages/src/polklibrary/type/coursepages/browser/canvas_view.py
@@ -35,7 +35,7 @@ class CanvasView(BrowserView):
     SUSTAINABILITY = ['sustainable','sustainability','energy and facilities management']
     
     
-    NON_EDITOR_ROLES = ['student', 'interpreter pre-semester', 'observers', 'interpreter semester', 'learner']
+    NON_EDITOR_ROLES = ['student', 'interpreter pre-semester', 'observers', 'interpreter semester', 'learner', 'students']
     
     def __call__(self):
         self.message = None
