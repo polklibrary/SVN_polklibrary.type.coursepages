3bbfb7e43c321b6f49deab2001e94a8ddf6f5a15
hietpasd <hietpasd@uwosh.edu>
2022-01-05 09:36:21 -0600

    removed indexer

--- polklibrary.type.coursepages/src/polklibrary/type/coursepages/models/page.py
+++ polklibrary.type.coursepages/src/polklibrary/type/coursepages/models/page.py
@@ -42,14 +42,14 @@ class IPage(model.Schema):
             missing_value=True,
         )
         
-@indexer(IPage)
-def make_searchable(object, **kwargs):
-    import re
-    portal_transforms = api.portal.get_tool(name='portal_transforms')
-    data = portal_transforms.convertTo('text/plain', object.body.output, mimetype='text/structured')
-    text = data.getData()
-    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', object.body.output)
-    return [object.title, object.description, text] + urls
+# @indexer(IPage)
+# def make_searchable(object, **kwargs):
+    # import re
+    # portal_transforms = api.portal.get_tool(name='portal_transforms')
+    # data = portal_transforms.convertTo('text/plain', object.body.output, mimetype='text/structured')
+    # text = data.getData()
+    # urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', object.body.output)
+    # return [object.title, object.description, text] + urls
         
         
         
